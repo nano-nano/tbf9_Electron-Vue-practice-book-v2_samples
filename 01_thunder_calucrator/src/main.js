1fff26861a4a683564b98809fafab26064c6faa5
nano-nano <nano2.aloerina@gmail.com>
2020-06-19 22:38:49 +0900

    01_CSSフレームワークを使うための設定

--- 01_thunder_calucrator/src/main.js
+++ 01_thunder_calucrator/src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// CSS(bootstrap-honoka)のロード
+import 'bootstrap-honoka/dist/css/bootstrap.css'
+
+// bootstrap-vueライブラリのロード
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
