70cc7de24b922de4f98982fd85b9aa2c8c1b6d12
nano-nano <nano2.aloerina@gmail.com>
2020-06-23 16:48:46 +0900

    02_最低限の実装まで削減、CSSフレームワーク導入

--- 02_thinking_face_game/src/main.js
+++ 02_thinking_face_game/src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import 'bootstrap-honoka/dist/css/bootstrap.css'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 new Vue({
   router,
   render: h => h(App)
