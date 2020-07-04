eda47f717780d3f50592e57b7dffac653f5fcfb5
nano-nano <nano2.aloerina@gmail.com>
2020-07-04 19:47:27 +0900

    03_メイン画面の仮UI実装

---
+++ 03_quiz_projector/src/components/ImportedFileView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="importedFileView card">
+      <div class="card-body">
+        <div class="h4">問題データファイル</div>
+        <div class="d-flex">
+            <div class="flex-grow-1 text-truncate" style="align-self: center">path...aaaaaaaaaaaaaaaaaaaaaaaa</div>
+            <b-button variant="primary" class="text-nowrap">ファイル選択</b-button>
+        </div>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.importedFileView {
+    background-color: #e7e7e7;
+}
+</style>
--- 03_quiz_projector/src/views/Home.vue
+++ 03_quiz_projector/src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="home">
-    <h1>This is Home</h1>
+    <!-- ファイルインポートコンポーネント -->
+    <ImportedFileView class="m-2"/>
+    <!-- 問題プレビューエリア -->
+    <div class="d-flex justify-content-between mx-2 my-4">
+      <b-button variant="outline-primary" size="lg" class="text-nowrap">＜＜前の問題</b-button>
+      <div class="card question-area">
+        <div class="card-body">
+          <div class="font-weight-bold">【問題】</div>
+          <div>もんだいぶんダミーあああああああああああああああああああああああああああああああああああ</div>
+          <div class="font-weight-bold mt-4">【答え】</div>
+          <div>こたえダミーあああああああああああああああああああああああああああああああああああ</div>
+        </div>
+      </div>
+      <b-button variant="outline-primary" size="lg" class="text-nowrap">次の問題＞＞</b-button>
+    </div>
+    <!-- オペレーションエリア -->
+    <div class="d-flex justify-content-center mx-2 my-4">
+      <b-button variant="outline-primary" size="lg" class="mx-3">問題を表示</b-button>
+      <b-button variant="outline-primary" size="lg" class="mx-3">解答を表示</b-button>
+      <b-button variant="outline-primary" size="lg" class="mx-3">表示をすべて消す</b-button>
+      <b-button variant="outline-secondary" size="lg" class="ml-5">表示設定</b-button>
+    </div>
   </div>
 </template>
 
 <script>
+import ImportedFileView from '@/components/ImportedFileView.vue'
+
 export default {
   name: 'Home',
   components: {
+    ImportedFileView
   }
 }
 </script>
+
+<style scoped>
+.question-area {
+  height: 50vh;
+  width: 55vw;
+}
+</style>
